1c23c30e5136861df545d4fbf0ac65d49c150bbb
koushith <koushith97@gmail.com>
2025-04-24 13:07:12 +0530

    fear=t: adds crypto blog

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+// @ts-nocheck
 import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import type { UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import removeConsole from 'vite-plugin-remove-console';
 
@@ -24,4 +26,21 @@ export default defineConfig(({ mode }) => ({
       '@assets': '/src/assets',
     },
   },
-}));
+
+  // For Vite 4.x and above, use this
+  preview: {
+    historyApiFallback: true,
+  },
+
+  // For development server
+  server: {
+    middleware: [
+      (req, res, next) => {
+        if (req.url?.endsWith('.html')) {
+          req.url = '/';
+        }
+        next();
+      },
+    ],
+  },
+})) as UserConfig;

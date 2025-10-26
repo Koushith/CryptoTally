176a57af9ac586c9ce5ce07b518115c5ae9d97af
koushith <koushith97@gmail.com>
2025-10-26 10:55:31 +0530

    fix: config issues

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,15 +1,14 @@
 import dotenv from 'dotenv';
 
-//const useProdEnvFile = false; //  Change to false when developing locally
-
-export const isDevelopment = process.env.NODE_ENV === 'development';
-export const isProduction = process.env.NODE_ENV === 'production';
-// Load .env.local
-dotenv.config({ path: isDevelopment ? '.env.local' : '.env' });
+// Load .env.local first (for local development), fall back to .env (for production)
+dotenv.config({ path: '.env.local' });
 
 export const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || '5000',
+  PORT: process.env.PORT || '8000',
   DATABASE_URL: process.env.DATABASE_URL || '',
   PROD_DATABASE_URL: process.env.PROD_DATABASE_URL || '',
 } as const;
+
+export const isDevelopment = env.NODE_ENV === 'development';
+export const isProduction = env.NODE_ENV === 'production';
